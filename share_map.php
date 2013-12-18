df860fef9240242779af9b476c85416712573ca4
Sparkmasterflex <raymondke99@gmail.com>
2013-12-18 17:42:28 -0500

    SVG temporary fix
    commented out .svg in bg-images
    
    removed commented out code in share_map.php

--- share_map.php
+++ share_map.php
@@ -2,7 +2,6 @@
   $data     = $_POST['content'];
   $filename = $_POST['filename'];
 
-  // $file = fopen("/javascripts/saved_data/$filename", 'w') or die("can't open file");
   file_put_contents($_SERVER['DOCUMENT_ROOT']."/javascripts/saved_data/$filename", $data);
 
   header('Content-type: application/json');
